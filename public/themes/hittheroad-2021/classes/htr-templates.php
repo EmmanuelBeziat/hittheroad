0481ed18a6054439a80c6351a0071400bc3a47d1
Emmanuel Béziat <emmanuel@italic.fr>
2021-10-11 15:34:00 +0200

    scrollbars

--- public/themes/hittheroad-2021/classes/htr-templates.php
+++ public/themes/hittheroad-2021/classes/htr-templates.php
@@ -32,6 +32,12 @@ class HTR_Templates {
 	 * Add custom sizes for images.
 	 */
 	public function image_sizes() {
+		update_option('thumbnail_size_h', 0);
+		update_option('thumbnail_size_w', 0);
+		update_option('medium_size_h', 0);
+		update_option('medium_size_w', 0);
+		update_option('large_size_h', 0);
+		update_option('large_size_w', 0);
 	}
 }
 
