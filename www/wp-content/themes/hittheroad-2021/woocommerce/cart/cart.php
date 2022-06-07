cb7f3c3965b05de822cb59d8e54c0ad0e9fe47aa
Emmanuel Béziat <emmanuel@italic.fr>
2022-06-08 01:25:18 +0200

    update cart

--- www/wp-content/themes/hittheroad-2021/woocommerce/cart/cart.php
+++ www/wp-content/themes/hittheroad-2021/woocommerce/cart/cart.php
@@ -26,7 +26,6 @@ defined('ABSPATH') || exit;
 		<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 			<thead>
 				<tr>
-					<th class="product-remove">&nbsp;</th>
 					<th class="product-thumbnail">&nbsp;</th>
 					<th class="product-name"><?php esc_html_e('Product', 'woocommerce'); ?></th>
 					<th class="product-price"><?php esc_html_e('Price', 'woocommerce'); ?></th>
@@ -46,22 +45,6 @@ defined('ABSPATH') || exit;
 						$product_permalink = apply_filters('woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink($cart_item) : '', $cart_item, $cart_item_key);
 						?>
 						<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
-
-							<td class="product-remove">
-								<?php
-									echo apply_filters(// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-										'woocommerce_cart_item_remove_link',
-										sprintf(
-											'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-											esc_url(wc_get_cart_remove_url($cart_item_key)),
-											esc_html__('Remove this item', 'woocommerce'),
-											esc_attr($product_id),
-											esc_attr($_product->get_sku())
-										),
-										$cart_item_key
-									);
-								?>
-							</td>
 
 							<td class="product-thumbnail">
 							<?php
@@ -102,27 +85,40 @@ defined('ABSPATH') || exit;
 							</td>
 
 							<td class="product-quantity" data-title="<?php esc_attr_e('Quantity', 'woocommerce'); ?>">
-							<?php
-							if ($_product->is_sold_individually()) {
-								$product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1">', $cart_item_key);
-							} else {
-								$product_quantity = woocommerce_quantity_input(
-									array(
-										'input_name'   => "cart[{$cart_item_key}][qty]",
-										'input_value'  => $cart_item['quantity'],
-										'max_value'    => $_product->get_max_purchase_quantity(),
-										'min_value'    => '0',
-										'product_name' => $_product->get_name(),
+								<div class="d-flex align-items-center">
+								<?php
+								if ($_product->is_sold_individually()) {
+									$product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1">', $cart_item_key);
+								} else {
+									$product_quantity = woocommerce_quantity_input(
+										array(
+											'input_name'   => "cart[{$cart_item_key}][qty]",
+											'input_value'  => $cart_item['quantity'],
+											'max_value'    => $_product->get_max_purchase_quantity(),
+											'min_value'    => '0',
+											'product_name' => $_product->get_name(),
+										),
+										$_product,
+										false
+									);
+								}
+
+								echo apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item); // PHPCS: XSS ok.
+
+								echo apply_filters(// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+									'woocommerce_cart_item_remove_link',
+									sprintf(
+										'<a href="%s" class="remove ms-1" aria-label="%s" data-product_id="%s" data-product_sku="%s"><i class="fa fa-trash"></i></a>',
+										esc_url(wc_get_cart_remove_url($cart_item_key)),
+										esc_html__('Remove this item', 'woocommerce'),
+										esc_attr($product_id),
+										esc_attr($_product->get_sku())
 									),
-									$_product,
-									false
+									$cart_item_key
 								);
-							}
-
-							echo apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item); // PHPCS: XSS ok.
-							?>
+								?>
+								</div>
 							</td>
-
 							<td class="product-subtotal" data-title="<?php esc_attr_e('Subtotal', 'woocommerce'); ?>">
 								<?php
 									echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // PHPCS: XSS ok.
