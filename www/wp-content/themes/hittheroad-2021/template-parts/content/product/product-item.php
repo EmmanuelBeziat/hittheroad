d65c3b128e9fbf299eb2f6dd46edcd4be6fd4176
Emmanuel Béziat <emmanuel@italic.fr>
2022-06-09 18:00:06 +0200

    debug product items

--- www/wp-content/themes/hittheroad-2021/template-parts/content/product/product-item.php
+++ www/wp-content/themes/hittheroad-2021/template-parts/content/product/product-item.php
@@ -1,6 +1,6 @@
 <?php $product_id = $args['id']; ?>
 <article class="product swiper-slide">
-	<a href="<?= get_the_permalink(get_the_ID($product_id)) ?>" class="product-link">
+	<a href="<?= get_the_permalink($product_id) ?>" class="product-link">
 		<h2 class="screen-reader-text"><?= get_the_title($product_id); ?></h2>
 		<div class="product-picture">
 			<img src="<?= wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'product-thumbnail')[0] ?>" alt>
