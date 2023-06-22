ba270f6e732eb0e21d0c2ebc2b21452e94d69e3c
Emmanuel Béziat <contact@emmanuelbeziat.com>
2023-06-22 05:30:22 +0200

    debug orders

--- www/wp-content/themes/hittheroad-2021/woocommerce/myaccount/orders.php
+++ www/wp-content/themes/hittheroad-2021/woocommerce/myaccount/orders.php
@@ -98,7 +98,7 @@ do_action('woocommerce_before_account_orders', $has_orders); ?>
 <?php else : ?>
 	<div>
 		<div class="alert alert-secondary mb-2" role="alert">
-			<?php esc_html_e('No order has been made yet.', 'woocommerce')); ?>
+			<?php esc_html_e('No order has been made yet.', 'woocommerce'); ?>
 		</div>
 		<a class=" btn btn-primary" href="<?= esc_url(apply_filters('woocommerce_return_to_shop_redirect', wc_get_page_permalink('shop'))); ?>"><?php wc_print_notice(esc_html__('Browse products', 'woocommerce'), 'notice'); ?></a>
 	</div>
