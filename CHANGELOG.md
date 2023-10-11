320ecb47177fa4f33126acde40faac81cb55b504
Emmanuel Béziat <contact@emmanuelbeziat.com>
2023-10-11 18:20:22 +0200

    Update woocommerce

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
 # Suivi de mises à jour
 
+## 11.10.2023
+
+* Mise à jour templates woocommerce
+  * `hittheroad-2021/woocommerce/checkout/payment.php` 7.8.0 → 8.1.0
+  * `hittheroad-2021/woocommerce/checkout/thankyou.php` 3.7.0 → 8.1.0
+
 ## 09.08.2023
 
 * Mise à jour Wordpress 6.3
-* Mise à jour templates woocomemrce
+* Mise à jour templates woocommerce
   * `hittheroad-2021/woocommerce/cart/cart.php` 7.8.0 → 7.9.0
--- www/wp-content/themes/hittheroad-2021/woocommerce/checkout/thankyou.php
+++ www/wp-content/themes/hittheroad-2021/woocommerce/checkout/thankyou.php
@@ -12,7 +12,9 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.7.0
+ * @version 8.1.0
+ *
+ * @var WC_Order $order
  */
 
 defined('ABSPATH') || exit;
@@ -42,8 +44,7 @@ defined('ABSPATH') || exit;
 			</p>
 
 		<?php else : ?>
-
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your order has been received.', 'woocommerce'), $order); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php wc_get_template('checkout/order-received.php', ['order' => $order]); ?>
 
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
@@ -77,16 +78,12 @@ defined('ABSPATH') || exit;
 				<?php endif; ?>
 
 			</ul>
-
 		<?php endif; ?>
 
 		<?php do_action('woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id()); ?>
 		<?php do_action('woocommerce_thankyou', $order->get_id()); ?>
 
 	<?php else : ?>
-
-		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your order has been received.', 'woocommerce'), null); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-
+		<?php wc_get_template('checkout/order-received.php', ['order' => $order]); ?>
 	<?php endif; ?>
-
 </div>
